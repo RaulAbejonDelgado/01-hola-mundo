b687b5d7b10c32a569fb43990397ef0310226771
RaulAbejonDelgado <raul.abejon.delgado@gmail.com>
2018-06-09 01:36:12 +0200

    primera app angular seccion3

---
+++ src/app/components/body/body.component.ts
@@ -0,0 +1,18 @@
+
+import {Component} from'@angular/core';
+
+@Component({
+  selector:'app-body',
+  templateUrl:'./body.component.html',
+})
+export class BodyComponent{
+
+  mostrar:true;
+  frase:any = {
+    mensaje:'Experto en versos no escritos',
+    autor:'Raul'
+  };
+
+  personajes:string[] = ['Guille','Ryu','Ken']
+
+}
